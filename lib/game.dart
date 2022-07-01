020f00299a10a1ce3ccd2eac498fccf009e2ea5f
Josafá Souza Jr <josafassj@gmail.com>
2022-07-01 11:43:07 -0300

    Game over screen

--- lib/game.dart
+++ lib/game.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:game_2048/model/block.dart';
 import 'package:game_2048/model/enum_block.dart';
@@ -54,6 +55,7 @@ class _GameBoardState extends State<GameBoard> with TickerProviderStateMixin {
   ValueNotifier<num> points = ValueNotifier(0);
   int movesCounter = 0;
   bool hasMoved = false;
+  bool gameOver = false;
 
   @override
   void didChangeDependencies() {
@@ -284,12 +286,14 @@ class _GameBoardState extends State<GameBoard> with TickerProviderStateMixin {
       hasMoved = false;
       setNextPosition();
     }
-    checkEndGame();
+
+    isGameOver();
   }
 
-  checkEndGame() {
-    var isEnd = checkAvalibleMoves();
-    debugPrint("the game is over? $isEnd");
+  isGameOver() {
+    setState(() => {
+      gameOver = !checkAvalibleMoves()
+    });
   }
 
   bool checkAvalibleMoves() {
@@ -311,9 +315,35 @@ class _GameBoardState extends State<GameBoard> with TickerProviderStateMixin {
         }
       }
     }
-
     return false;
   }
+
+  // showGameOver() {
+  //   Widget newGameButton = TextButton(
+  //     child: Text("New Game"),
+  //     onPressed: () => newGame(),
+  //   );
+  //   Widget finishGameButton = TextButton(
+  //     child: Text("Finish"),
+  //     onPressed: () {},
+  //   );
+  //
+  //   AlertDialog alert = AlertDialog(
+  //     title: Text("Game Over"),
+  //     content: Text("Create a new game?"),
+  //     actions: [
+  //       newGameButton,
+  //       finishGameButton,
+  //     ],
+  //   );
+  //
+  //   showDialog(
+  //     context: context,
+  //     builder: (BuildContext context) {
+  //       return alert;
+  //     },
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -326,168 +356,228 @@ class _GameBoardState extends State<GameBoard> with TickerProviderStateMixin {
     DragUpdateDetails? updateHorizontalDragDetails;
 
     return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 253, 247, 240),
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 50),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    height: 120,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5.0),
-                        color: BlockNumber.NINE.color),
-                    child: const Center(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 20),
-                        child: Text(
-                          "2048",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 64,
-                              fontWeight: FontWeight.bold),
+      backgroundColor: const Color.fromARGB(255, 253, 247, 240),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      height: 120,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          color: BlockNumber.NINE.color),
+                      child: const Center(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          child: Text(
+                            "2048",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 64,
+                                fontWeight: FontWeight.bold),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 140,
-                        margin: const EdgeInsets.only(bottom: 10),
-                        decoration: const BoxDecoration(
-                            color: Color.fromARGB(255, 240, 228, 218)),
-                        child: Column(
-                          children: [
-                            const Text(
-                              "Points",
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 5),
-                              child: ValueListenableBuilder(
-                                valueListenable: points,
-                                builder: (context, value, child) => Text(
-                                  "${points.value}",
-                                  style: const TextStyle(
-                                      fontSize: 40,
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 140,
+                          margin: const EdgeInsets.only(bottom: 10),
+                          decoration: const BoxDecoration(
+                              color: Color.fromARGB(255, 240, 228, 218)),
+                          child: Column(
+                            children: [
+                              const Text(
+                                "Points",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 5),
+                                child: ValueListenableBuilder(
+                                  valueListenable: points,
+                                  builder: (context, value, child) => Text(
+                                    "${points.value}",
+                                    style: const TextStyle(
+                                        fontSize: 40,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                      Row(
-                        children: [
-                          TextButton(
-                              style: ButtonStyle(
-                                foregroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        Colors.white),
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        BlockNumber.ELEVEN.color),
-                              ),
-                              onPressed: () => newGame(),
-                              child: const Text("New Game")),
-                        ],
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              height: boardSize,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5.0),
-                  color: const Color(0xffb9aea0)),
-              margin: const EdgeInsets.symmetric(horizontal: 10),
-              child: GestureDetector(
-                onVerticalDragStart: (dragDetails) {
-                  startVerticalDragDetails = dragDetails;
-                },
-                onVerticalDragUpdate: (dragDetails) {
-                  updateVerticalDragDetails = dragDetails;
-                },
-                onVerticalDragEnd: (endDetails) {
-                  double dx = updateVerticalDragDetails!.globalPosition.dx -
-                      startVerticalDragDetails!.globalPosition.dx;
-                  double dy = updateVerticalDragDetails!.globalPosition.dy -
-                      startVerticalDragDetails!.globalPosition.dy;
-                  double velocity = endDetails.primaryVelocity!;
-                  //Convert values to be positive
-                  if (dx < 0) dx = -dx;
-                  if (dy < 0) dy = -dy;
-                  double positiveVelocity = velocity < 0 ? -velocity : velocity;
-                  if (dx > verticalSwipeMaxWidthThreshold) return;
-                  if (dy < verticalSwipeMinDisplacement) return;
-                  if (positiveVelocity < verticalSwipeMinVelocity) return;
-                  if (velocity < 0) {
-                    swipe(Direction.up);
-                  } else {
-                    swipe(Direction.down);
-                  }
-                },
-                onHorizontalDragStart: (dragDetails) {
-                  startHorizontalDragDetails = dragDetails;
-                },
-                onHorizontalDragUpdate: (dragDetails) {
-                  updateHorizontalDragDetails = dragDetails;
-                },
-                onHorizontalDragEnd: (endDetails) {
-                  double dx = updateHorizontalDragDetails!.globalPosition.dx -
-                      startHorizontalDragDetails!.globalPosition.dx;
-
-                  double dy = updateHorizontalDragDetails!.globalPosition.dy -
-                      startHorizontalDragDetails!.globalPosition.dy;
-
-                  double velocity = endDetails.primaryVelocity!.toDouble();
-
-                  if (dx < 0) dx = -dx;
-                  if (dy < 0) dy = -dy;
-                  double positiveVelocity = velocity < 0 ? -velocity : velocity;
-
-                  if (dx < horizontalSwipeMinDisplacement) return;
-                  if (dy > horizontalSwipeMaxHeightThreshold) return;
-                  if (positiveVelocity < horizontalSwipeMinVelocity) return;
-
-                  if (velocity < 0) {
-                    swipe(Direction.left);
-                  } else {
-                    swipe(Direction.right);
-                  }
-                },
-                child: GridView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: widget.blocksPerLine,
-                    childAspectRatio: 1.0,
-                    mainAxisSpacing: 5.0,
-                    crossAxisSpacing: 5.0,
-                  ),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5.0, horizontal: 0),
-                  itemCount: widget.blocksPerLine * widget.blocksPerLine,
-                  itemBuilder: (context, index) {
-                    var x = (index / widget.blocksPerLine).floor();
-                    var y = index % widget.blocksPerLine;
-                    return grid[x][y].widget;
-                  },
+                        Row(
+                          children: [
+                            TextButton(
+                                style: ButtonStyle(
+                                  foregroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.white),
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          BlockNumber.ELEVEN.color),
+                                ),
+                                onPressed: () => newGame(),
+                                child: const Text("New Game")),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
                 ),
               ),
-            ),
-            const Text("Monetization"),
-          ],
-        ));
+              Container(
+                height: boardSize,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5.0),
+                    color: const Color(0xffb9aea0)),
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                child: GestureDetector(
+                  onVerticalDragStart: (dragDetails) {
+                    startVerticalDragDetails = dragDetails;
+                  },
+                  onVerticalDragUpdate: (dragDetails) {
+                    updateVerticalDragDetails = dragDetails;
+                  },
+                  onVerticalDragEnd: (endDetails) {
+                    double dx = updateVerticalDragDetails!.globalPosition.dx -
+                        startVerticalDragDetails!.globalPosition.dx;
+                    double dy = updateVerticalDragDetails!.globalPosition.dy -
+                        startVerticalDragDetails!.globalPosition.dy;
+                    double velocity = endDetails.primaryVelocity!;
+                    //Convert values to be positive
+                    if (dx < 0) dx = -dx;
+                    if (dy < 0) dy = -dy;
+                    double positiveVelocity =
+                        velocity < 0 ? -velocity : velocity;
+                    if (dx > verticalSwipeMaxWidthThreshold) return;
+                    if (dy < verticalSwipeMinDisplacement) return;
+                    if (positiveVelocity < verticalSwipeMinVelocity) return;
+                    if (velocity < 0) {
+                      swipe(Direction.up);
+                    } else {
+                      swipe(Direction.down);
+                    }
+                  },
+                  onHorizontalDragStart: (dragDetails) {
+                    startHorizontalDragDetails = dragDetails;
+                  },
+                  onHorizontalDragUpdate: (dragDetails) {
+                    updateHorizontalDragDetails = dragDetails;
+                  },
+                  onHorizontalDragEnd: (endDetails) {
+                    double dx = updateHorizontalDragDetails!.globalPosition.dx -
+                        startHorizontalDragDetails!.globalPosition.dx;
+
+                    double dy = updateHorizontalDragDetails!.globalPosition.dy -
+                        startHorizontalDragDetails!.globalPosition.dy;
+
+                    double velocity = endDetails.primaryVelocity!.toDouble();
+
+                    if (dx < 0) dx = -dx;
+                    if (dy < 0) dy = -dy;
+                    double positiveVelocity =
+                        velocity < 0 ? -velocity : velocity;
+
+                    if (dx < horizontalSwipeMinDisplacement) return;
+                    if (dy > horizontalSwipeMaxHeightThreshold) return;
+                    if (positiveVelocity < horizontalSwipeMinVelocity) return;
+
+                    if (velocity < 0) {
+                      swipe(Direction.left);
+                    } else {
+                      swipe(Direction.right);
+                    }
+                  },
+                  child: GridView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: widget.blocksPerLine,
+                      childAspectRatio: 1.0,
+                      mainAxisSpacing: 5.0,
+                      crossAxisSpacing: 5.0,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 5.0, horizontal: 0),
+                    itemCount: widget.blocksPerLine * widget.blocksPerLine,
+                    itemBuilder: (context, index) {
+                      var x = (index / widget.blocksPerLine).floor();
+                      var y = index % widget.blocksPerLine;
+                      return grid[x][y].widget;
+                    },
+                  ),
+                ),
+              ),
+              const Text("Monetization"),
+            ],
+          ),
+          gameOver
+              ? Center(
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    alignment: Alignment.center,
+                    height: 200,
+                    width: 300,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5.0),
+                        color: const Color.fromRGBO(0, 0, 0, 0.5)),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 50),
+                          child: const Text("Game Over",
+                              style: TextStyle(
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white)),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            TextButton(
+                                style: ButtonStyle(
+                                  foregroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.white),
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          BlockNumber.ELEVEN.color),
+                                ),
+                                onPressed: () => newGame(),
+                                child: const Text("New Game")),
+                            TextButton(
+                                style: ButtonStyle(
+                                  foregroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.white),
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.red),
+                                ),
+                                onPressed: () => SystemNavigator.pop(),
+                                child: const Text("Finish")),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              : Container(),
+        ],
+      ),
+    );
   }
 }
